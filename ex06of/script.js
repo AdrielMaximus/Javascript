621ebdb4d0a1c8f1bfad434f84453d6640b6ad57
Adriel Maximus <adrvlogs885@gmail.com>
2023-02-02 13:33:11 -0300

    a

--- ex06of/script.js
+++ ex06of/script.js
@@ -1,7 +1,7 @@
-function verificar() {
+function v() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById("txtano")
+    var fano = document.getElementById(' t xtano')
     var res = document.querySelector('div#res')
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('ERRO')
@@ -9,5 +9,13 @@ function verificar() {
         var fsex = document.getElementByName("radsex")
         var idade = ano - Number(fano.value)
         res.innerHTML = `Idade calculada: ${idade}`
+        var img = document.createElement
+        var genero = ''
+        if(fsex[0].checked){
+            genero = 'homem'
+        } else if(fsex[1].checked){
+            genero = 'mulher'
+        }
+        res,style.textalign = 'center'
     }
 }
