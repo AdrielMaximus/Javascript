441063aa979fcf360cc0d4a8f77627cd45cd53e5
Adriel Maximus <adrvlogs885@gmail.com>
2023-02-03 14:30:56 -0300

    qa

--- ex06of/script.js
+++ ex06of/script.js
@@ -1,21 +1,20 @@
-function v() {
+function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById(' t xtano')
+    var fano = document.getElementById('t xtano')
     var res = document.querySelector('div#res')
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('ERRO')
     } else {
-        var fsex = document.getElementByName("radsex")
+        var fsex = document.getElementByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
-        var img = document.createElement
         var genero = ''
-        if(fsex[0].checked){
+        if(fsex[0].checked) {
             genero = 'homem'
         } else if(fsex[1].checked){
             genero = 'mulher'
         }
-        res,style.textalign = 'center'
+        res.style.textalign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
     }
 }
