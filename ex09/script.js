9e90ae797411814314e7f8883fa3b9c16e383292
Adriel Maximus <adrvlogs885@gmail.com>
2023-02-06 14:38:45 -0300

    a

--- ex09/script.js
+++ ex09/script.js
@@ -2,9 +2,12 @@ function Calcular(){
     var num = document.getElementById('num')
     var tab = document.getElementById('tabuada')
     var n = Number(num.value)
-    while (c <f) {
-        c += p
-        res.innerHTML += (` <br> ${c}`)
+    var c = 1
+    while (c <=10) {
+        var item = document.createElement('option')
+        item.text = `${num} x ${c} = ${num*c}`
+        tab.appendChild(item)
+        c++
     }
 
 }
