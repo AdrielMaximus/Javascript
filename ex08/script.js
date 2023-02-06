b0e84d18da918c1f485ca3cb34f1e6637cc6b779
Adriel Maximus <adrvlogs885@gmail.com>
2023-02-06 13:51:51 -0300

    a

--- ex08/script.js
+++ ex08/script.js
@@ -1,12 +1,11 @@
-/*
-    var c = 1
-    while (c <=5) {
+function Calcular(){
+    var ini = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var res = document.getElementById('res')
+    for(var c = 1;c <= 5; c++){
         console.log(c)
-        c++
     }
-*/
-console.log('Vai comecar')
-for(var c = 1;c <= 5; c++){
-    console.log(c)
 }
-console.log('FIM')
+
+
