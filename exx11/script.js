95005985f7b4346c65651dfb653cec463ebe4393
Adriel Maximus <adrvlogs885@gmail.com>
2023-02-09 16:47:25 -0300

    qa

---
+++ exx11/script.js
@@ -0,0 +1,21 @@
+var nume = document.getElementById('num')
+var num = Number(nume.value)
+var lista = document.getElementById('numeros')
+var res = document.getElementById('res')
+let valores = []
+function inlista(n, 1){
+
+}
+function adicionar(){
+    if (num == 0 || num == 101 ){
+        window.alert('ERRO, digite um numero valido')
+    } else if (inlista(num), valores){
+        window.alert('ERRO, digite um numero nao listado')
+    }
+/*let num = [5, 8, 4]
+num[3] = 6
+num.push(7)
+console.log(`O seu programa tem ${num.length} elementos que sao respectivamente:`)
+console.log(num.sort())
+console.log(num.indexOf(8))*/
+}
