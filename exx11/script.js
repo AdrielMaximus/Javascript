f37c9dd315bb58e378d43fd7e937c6745a44d3f5
Adriel Maximus <adrvlogs885@gmail.com>
2023-02-10 12:26:49 -0300

    aa

--- exx11/script.js
+++ exx11/script.js
@@ -1,8 +1,15 @@
-var nume = document.getElementById('num')
-var num = Number(nume.value)
+var num = document.getElementById('num')
 var lista = document.getElementById('numeros')
 var res = document.getElementById('res')
 let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+}
 function inlista(n, l){
     if(l.indexOf(Number(n)) != -1) {
         return true
@@ -11,12 +18,18 @@ function inlista(n, l){
     }
 }
 function adicionar(){
-    if (num <= 0 || num >= 101 ){
-        window.alert('ERRO, digite um numero valido')
-    } else if (inlista(num), valores){
-        window.alert('ERRO, digite um numero nao listado')
-    } else{
-        valores.push(num)
+    if (isNumero(num.value) && !inlista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} foi adicionado`
+        lista.appendChild(item)
+    } else {
+        window.alert('Valor invalido ou ja adicionado')
     }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
 
 }
