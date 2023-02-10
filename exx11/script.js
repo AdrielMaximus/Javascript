3dce2be1fe88d77e3768293963e3c07b021dbd25
Adriel Maximus <adrvlogs885@gmail.com>
2023-02-10 10:46:29 -0300

    Update script.js

--- exx11/script.js
+++ exx11/script.js
@@ -3,19 +3,18 @@ var num = Number(nume.value)
 var lista = document.getElementById('numeros')
 var res = document.getElementById('res')
 let valores = []
-function inlista(n, 1){
-
+function inlista(n, l){
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
 }
 function adicionar(){
-    if (num == 0 || num == 101 ){
+    if (num <= 0 || num >= 101 ){
         window.alert('ERRO, digite um numero valido')
     } else if (inlista(num), valores){
         window.alert('ERRO, digite um numero nao listado')
     }
-/*let num = [5, 8, 4]
-num[3] = 6
-num.push(7)
-console.log(`O seu programa tem ${num.length} elementos que sao respectivamente:`)
-console.log(num.sort())
-console.log(num.indexOf(8))*/
+
 }
