4f844f48342d4a88c2d37e8b35d16da052bea12b
Adriel Maximus <adrvlogs885@gmail.com>
2023-02-10 12:44:06 -0300

    a

--- exx11/script.js
+++ exx11/script.js
@@ -23,6 +23,7 @@ function adicionar(){
         let item = document.createElement('option')
         item.text = `Valor ${num.value} foi adicionado`
         lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor invalido ou ja adicionado')
     }
@@ -31,5 +32,24 @@ function adicionar(){
 }
 
 function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione um valor antes de finalizar')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        for(let pos in valores){
+            if (valores[pos] > maior){
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor){
+                menor = valores[pos]
+            }
 
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo tempos ${tot} numeros cadastrados</p>`
+        res.innerHTML += `<p>O maior valor foi ${maior} </p>`
+        res.innerHTML += `<p>O menor valor foi ${menor} </p>`
+    }
 }
